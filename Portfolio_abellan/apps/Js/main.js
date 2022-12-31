1aac1e14172cc75d57a7bc8bb4048096c31b82b2
Elerinna2a <elerinna2a@gmail.com>
2022-12-31 01:20:05 +0100

    mon portfolio

---
+++ Portfolio_abellan/apps/Js/main.js
@@ -0,0 +1,17 @@
+// const menu = document.querySelector(".nav__menu1");
+// const openMenu = document.querySelector("#open-btn");
+// const closeMenu = document.querySelector("#close-btn");
+
+// openMenu.addEventListener("click", () => {
+//   menu.style.display = "flex";
+//   openMenu.style.display = "none";
+//   closeMenu.style.display = "block";
+// });
+
+// const closeTheMenu = () => {
+//   menu.style.display = "none";
+//   openMenu.style.display = "flex";
+//   closeMenu.style.display = "none";
+// };
+
+// closeMenu.addEventListener("click", closeTheMenu);

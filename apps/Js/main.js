f166c732d5dbac535df11e276ffcdac38a234511
Elerinna2a <elerinna2a@gmail.com>
2023-01-01 17:57:55 +0100

    withjs

--- apps/Js/main.js
+++ apps/Js/main.js
@@ -15,3 +15,27 @@
 // };
 
 // closeMenu.addEventListener("click", closeTheMenu);
+
+//  ========== slide img ============
+//  initialisation des variable
+var i = 0;
+var images = ["/img/img1.jpg", "/img/img2.png", "/img/img3.jpg"];
+var time = 3000;
+
+// Definition de la fonction permettant le slide en boucle
+// qui se base sur le tableau d'images fait plus haut
+
+function changeImg() {
+  document.slide.src = images[i];
+
+  if (i < images.length - 1) {
+    i++;
+  } else {
+    i = 0;
+  }
+  // setTimeout qui permet d'implementer un timer
+  //   & qui relance la fonction en mode recursif avec la variable time
+  setTimeout("changeImg()", time);
+}
+//  lancement du slide au chargement de la page
+window.onload = changeImg;

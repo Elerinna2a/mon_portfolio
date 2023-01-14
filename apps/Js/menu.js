d6630b088961a4aa04159075611b69475dd89f3d
Elerinna2a <elerinna2a@gmail.com>
2023-01-14 20:28:57 +0100

    new merge

--- apps/Js/menu.js
+++ apps/Js/menu.js
@@ -2,16 +2,60 @@ const menu = document.querySelector(".nav__menu");
 const openMenu = document.querySelector(".open__btn__menu");
 const closeMenu = document.querySelector(".close__btn__menu");
 
-openMenu.addEventListener("click", () => {
-  menu.style.display = "flex";
-  openMenu.style.display = "none";
-  closeMenu.style.display = "block";
+// openMenu.addEventListener("click", () => {
+//   menu.style.left = "0px";
+//   openMenu.style.display = "none";
+//   closeMenu.style.display = "block";
+// });
+
+// const closeTheMenu = () => {
+//   menu.style.left = "-1024px";
+//   openMenu.style.display = "block";
+//   closeMenu.style.display = "none";
+// };
+// closeMenu.addEventListener("click", closeTheMenu);
+
+// window.addEventListener("resize", function () {
+//   if (window.innerWidth > 1024) {
+//     openMenu.style.display = "none";
+//   } else {
+//     openMenu.style.display = "block";
+//   }
+// });
+
+let menuOpen = false;
+const linkAbout = document.querySelector("#aboutMenu");
+linkAbout.addEventListener("click", () => {
+  if (menuOpen) {
+    closeTheMenu();
+  }
+  const about = document.querySelector("#about");
+  about.scrollIntoView({ behavior: "smooth" });
 });
 
 const closeTheMenu = () => {
-  menu.style.display = "none";
+  menu.style.left = "-1024px";
   openMenu.style.display = "block";
   closeMenu.style.display = "none";
+  menuOpen = false;
 };
 
 closeMenu.addEventListener("click", closeTheMenu);
+
+openMenu.addEventListener("click", () => {
+  menu.style.left = "0px";
+  openMenu.style.display = "none";
+  closeMenu.style.display = "block";
+  menuOpen = true;
+});
+
+window.addEventListener("resize", function () {
+  if (window.innerWidth > 1024) {
+    openMenu.style.display = "none";
+    if (menuOpen) {
+      closeTheMenu();
+    }
+  } else {
+    openMenu.style.display = "block";
+  }
+});

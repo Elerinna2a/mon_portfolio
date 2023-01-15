0496781e5af05bbb6d5bc53d35d36f6637ddbdcb
Elerinna2a <elerinna2a@gmail.com>
2023-01-15 13:10:47 +0100

    Update menu.js

--- apps/Js/menu.js
+++ apps/Js/menu.js
@@ -3,27 +3,6 @@ const openMenu = document.querySelector(".open__btn__menu");
 const closeMenu = document.querySelector(".close__btn__menu");
 const about = document.getElementById("about");
 
-// openMenu.addEventListener("click", () => {
-//   menu.style.left = "0px";
-//   openMenu.style.display = "none";
-//   closeMenu.style.display = "block";
-// });
-
-// const closeTheMenu = () => {
-//   menu.style.left = "-1024px";
-//   openMenu.style.display = "block";
-//   closeMenu.style.display = "none";
-// };
-// closeMenu.addEventListener("click", closeTheMenu);
-
-// window.addEventListener("resize", function () {
-//   if (window.innerWidth > 1024) {
-//     openMenu.style.display = "none";
-//   } else {
-//     openMenu.style.display = "block";
-//   }
-// });
-
 const closeTheMenu = () => {
   menu.style.left = "-1024px";
   openMenu.style.display = "block";
@@ -33,30 +12,37 @@ const closeTheMenu = () => {
 
 closeMenu.addEventListener("click", closeTheMenu);
 
-let menuOpen = false;
-const linkAbout = document.querySelector("#aboutMenu");
+let menuOpen = false; // Initialise la variable menuOpen à false pour indiquer que le menu est fermé
+
+const linkAbout = document.querySelector("#aboutMenu"); // Sélectionne l'élément de menu "à propos"
 linkAbout.addEventListener("click", () => {
+  // Ajoute un écouteur d'événement de clic à l'élément de menu
   if (menuOpen) {
-    closeTheMenu();
+    // Si le menu est ouvert
+    closeTheMenu(); // Appelle la fonction closeTheMenu pour fermer le menu
   }
-  const about = document.querySelector("#about");
-  about.scrollIntoView({ behavior: "smooth" });
+  const about = document.querySelector("#about"); // Sélectionne l'élément de section "à propos"
+  about.scrollIntoView({ behavior: "smooth" }); // Fait défiler la page vers l'élément de section "à propos"
 });
 
 openMenu.addEventListener("click", () => {
-  menu.style.left = "0px";
-  openMenu.style.display = "none";
-  closeMenu.style.display = "block";
-  menuOpen = true;
+  // Ajoute un écouteur d'événement de clic à l'élément d'ouverture de menu
+  menu.style.left = "0px"; // Déplace le menu vers la gauche de l'écran
+  openMenu.style.display = "none"; // Masque l'élément d'ouverture de menu
+  closeMenu.style.display = "block"; // Affiche l'élément de fermeture de menu
+  menuOpen = true; // Met à jour la variable menuOpen pour indiquer que le menu est maintenant ouvert
 });
 
 window.addEventListener("resize", function () {
+  // Ajoute un écouteur d'événement de redimensionnement de fenêtre
   if (window.innerWidth > 1024) {
-    openMenu.style.display = "none";
+    // Si la largeur de la fenêtre est supérieure à 1024px
+    openMenu.style.display = "none"; // Masque l'élément d'ouverture de menu
     if (menuOpen) {
-      closeTheMenu();
+      // Si le menu est ouvert
+      closeTheMenu(); // Appelle la fonction closeTheMenu pour fermer le menu
     }
   } else {
-    openMenu.style.display = "block";
+    openMenu.style.display = "block"; // Affiche l'élément d'ouverture de menu
   }
 });
